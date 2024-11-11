09ea2230cf2f425dac99a6e8d2f788751a10ae3b
gkoz <genadi.kozarev+github@gmail.com>
2024-11-11 15:02:05 +0000

    display matched results

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const endpoint =
     'https://gist.githubusercontent.com/GenadiKozarev/2e69b5f26527e3de0c464f15753ac77e/raw/0cc0086c0a8a1485faf1cc9b44a2dbed37d59ad5/usa_cities.json';
-
 const searchFrom = [];
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
 fetch(endpoint)
     .then(res => res.json())
@@ -17,3 +18,20 @@ function findMatches(query, searchFrom) {
         return result.city.match(regExp) || result.state.match(regExp);
     });
 }
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, searchFrom);
+    const html = matchArray
+        .map(result => {
+            return `
+            <li>
+                <span class="name">${result.city}, ${result.state}</span>
+                <span class="population">${result.population}</span>
+            </li>
+        `;
+        })
+        .join('');
+    suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener('keyup', displayMatches);
