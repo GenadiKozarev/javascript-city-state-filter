05e40f1e55e93b87ea8f848bd3d78d744973bf11
gkoz <genadi.kozarev+github@gmail.com>
2024-11-11 14:47:28 +0000

    fetch the data and find a match

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const endpoint =
     'https://gist.githubusercontent.com/GenadiKozarev/2e69b5f26527e3de0c464f15753ac77e/raw/0cc0086c0a8a1485faf1cc9b44a2dbed37d59ad5/usa_cities.json';
+
+const searchFrom = [];
+
+fetch(endpoint)
+    .then(res => res.json())
+    .then(data =>
+        data ? searchFrom.push(...data) : console.log('Missing data...')
+    );
+
+function findMatches(query, searchFrom) {
+    return searchFrom.filter(result => {
+        // 'g' - global flag ensures the search doesn't stop after the first query match
+        // 'i' - insensitive flag means the search will be case-insensitive
+        const regExp = new RegExp(query, 'gi');
+        return result.city.match(regExp) || result.state.match(regExp);
+    });
+}
